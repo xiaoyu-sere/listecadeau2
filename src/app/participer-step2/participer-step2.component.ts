c870a39e818d7107f0e72e89234085bfd977a50d
Xiaoyu Zhang <xiaoyu.sere@gmail.com>
2017-02-08 22:14:07 +0100

    Mise à jour page participation 2

--- src/app/participer-step2/participer-step2.component.ts
+++ src/app/participer-step2/participer-step2.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ListeService} from "../services/liste.service";
+import {Router, ActivatedRoute, Params} from "@angular/router";
+import {AngularFire} from "angularfire2";
+import {Liste} from "../model/liste";
 
 @Component({
   selector: 'app-participer-step2',
@@ -6,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParticiperStep2Component implements OnInit {
 
-  constructor() { }
+  token = '';
+  loading = false;
+  liste = new Liste();
+
+  constructor(private listeService: ListeService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private af: AngularFire,
+              private location: Location) {
+  }
 
   ngOnInit() {
+    this.route.params.switchMap((params: Params) => this.token = params['token']).subscribe(x => this.updateFilter());
+  }
+
+  updateFilter() {
+    if (this.listeService.listes != null) {
+      for (let uneListe of this.listeService.listes) {
+        if (this.token == uneListe.$key) {
+          this.liste = uneListe;
+          this.loading = false;
+        }
+      }
+    } else {
+      this.listeService.items.subscribe(x => this.updateFilter());
+    }
   }
 
 }
